a2d9c2e591d0a433049cd6b70474ae3da9f22d10
odgerey <audreymedaino@gmail.com>
2020-06-01 19:50:35 +0000

    Refactored the test code into separate test file...however this function does not work. The last two assertions are incorrect.

--- middle.js
+++ middle.js
@@ -1,29 +1,9 @@
-const eqArrays = function(firstArray, secondArray){
-  //Check if the firstArray share the same length
-  if (firstArray.length === secondArray.length){
-    for (let i = 0; i < firstArray.length; i++){
-        // if the content of firstArray i and content of firstArray j match
-        if (firstArray[i] != secondArray[i]){
-          return false
-        } 
-    }
-  return true
-  }
-}  
-
-const assertArraysEqual = function (actual, expected){ 
-  if(eqArrays(actual, expected)) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else{
-    console.log(`Assertion Failed: ${actual} !== ${expected}`)
-  }
-}  
-
 const middle = function(array) {
     const middleNumber = [];
     if (array.length <= 2){
       middleNumber.push([]);
-    } else if (array.length%1 === 0){
+      
+    } else if (array.length % 1 === 0) {
       const index = Math.round((array.length - 1) / 2)
       middleNumber.push(array[index]);
     } else if (array.length%2 === 0){
@@ -32,29 +12,5 @@ const middle = function(array) {
     } 
   return middleNumber
 }
-assertArraysEqual(middle([1]),[]);   // =>[]
-assertArraysEqual(middle([1, 2]), []); // => []
 
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-
-assertArraysEqual(middle([1, 2, 3, 4]), [2,3]); // => [2,3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]);// => [3,4]
-
-
-// create a function middle which will take in an array CHECK
-
-// return the middle-most elements of the given array
-
-//arrays with one or two elements = return empty...no middle CHECK
-//middle([1]) // => []
-//middle([1, 2]) // => []
-
-
-//arrays with odd number of elements, or an array containing a single middle element should be returned
-//middle([1, 2, 3]) // => [2]
-//middle([1, 2, 3, 4, 5]) // => [3]
-
-//arrays with even number of elements, or an array contianing the two elements in the middle should be returned
-//middle([1, 2, 3, 4]) // => [2, 3]
-//middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
+module.exports = middle;
