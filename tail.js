506942d3f001a420d1c869d57c5982c42f8ea4ff
odgerey <audreymedaino@gmail.com>
2020-06-01 18:50:41 +0000

    Refactored tail with test code in a test file

--- tail.js
+++ tail.js
@@ -1,52 +1,16 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`)
-  } else{
-    console.log(`Assertion Failed: ${actual} !== ${expected}`)
-  }
-};
-
-
-// const tail = function (actualArray) {
-//   let result = [];
-// for (let i = 1; i < actualArray.length; i++){
-//   result.push(actualArray[i]);
-// } return result;
-// }; actualArray=[1,2,5,7]
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// assertEqual
-// assertEqual(tail(actualArray.length, 2));
-// assertEqual(tail(actualArray[0],"Lighthouse"));
-// assertEqual(tail(actualArray[1], "Labs"));
-
-
-
-
-
 const tail = function (TEST) {
   let newArray = [];
 for (let i = 1; i < TEST.length; i++){
   newArray.push(TEST[i]);
 } return newArray;
-}; // let actualArray =[1,5,7,8]
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// assertEqual(tail(["Yo Yo", "Lighthouse","Labs"]));
-// assertEqual(TEST.length, 2);
-// assertEqual(TEST[0],"Lighthouse");
-// assertEqual(TEST[1], "Labs");
+}; 
 
-// Test Case: Check the original array 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+
 
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+module.exports = tail;
+module.exports = words;
+module.exports = result;
